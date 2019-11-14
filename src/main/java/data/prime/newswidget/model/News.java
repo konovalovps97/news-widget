b4fb428b7ab4e54a5e348535467398c98ccab415
konovalovps97 <311208Pashapasha1997>
2019-11-14 17:41:13 +0300

    add entity

--- src/main/java/data/prime/newswidget/model/News.java
+++ src/main/java/data/prime/newswidget/model/News.java
@@ -6,7 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.sql.Date;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -15,7 +15,7 @@ import java.util.List;
  * @author konovalovps97 at 09.11.2019
  */
 @Entity
-@Table(name = "NEWS")
+@Table(name = "ITEM_TYPE_TAB")
 @Data
 public class News {
 
@@ -23,36 +23,131 @@ public class News {
      * id новости
      */
     @Id
-    @Column(name = "ID_NEWS")
+    @Column(name = "ITM_ID")
     private Long id;
 
-    /**
-     * текст новости
-     */
-    @Column(name = "DATE_NEWS")
-    private Date date;
 
     /**
-     * текст новости
+     * id новости
      */
-    @Column(name = "TEXT_NEWS")
-    private String text;
+    @Column(name = "ITM_TYPE")
+    private String type;
 
     /**
-     * идентификатор ознакмления
+     * id новости
      */
-    @Column(name = "IS_FAMILIAR")
-    private boolean isFamiliar;
+    @Column(name = "ITM_SUB_TYPE")
+    private String subType;
 
     /**
-     * тема новости
+     * id новости
      */
-    @Column(name = "TOPIC_NEWS")
-    private String topic;
+    @Column(name = "ITM_ALIAS")
+    private String alias;
+
 
     /**
-     * тема новости
+     * id новости
      */
-    @Column(name = "TOPIC_SKILL")
-    private String skills;
+    @Column(name = "ITM_TITLE_SEARCH")
+    private String titleSearch;
+
+    /**
+     * id новости
+     */
+    @Column(name = "ITM_TITLE")
+    private String title;
+
+    /**
+     * id новости
+     */
+    @Column(name = "ITM_SUB_TITLE")
+    private String subTitle;
+
+    /**
+     * id новости
+     */
+    @Column(name = "ITM_EXTERNAL_ID")
+    private Long externalId;
+
+    /**
+     * id новости
+     */
+    @Column(name = "ITM_CREATE_DATE")
+    private Date createDate;
+
+    /**
+     * id новости
+     */
+    @Column(name = "ITM_CREATED_BY")
+    private String createdBy;
+
+    /**
+     * id новости
+     */
+    @Column(name = "ITM_UPDATE_DATE")
+    private Date update;
+
+    /**
+     * id новости
+     */
+    @Column(name = "ITM_UPDATED_BY")
+    private String updatedBy;
+
+    /**
+     * id новости
+     */
+    @Column(name = "ITM_PUBLISH_DATE")
+    private Date publishDate;
+
+    /**
+     * id новости
+     */
+    @Column(name = "ITM_EXPIRE_DATE")
+    private Date expireDate;
+
+    /**
+     * id новости
+     */
+    @Column(name = "ITM_HITS")
+    private Long hit;
+
+    /**
+     * id новости
+     */
+    @Column(name = "ITM_COUNT_HITS")
+    private Long countHit;
+
+    /**
+     * id новости
+     */
+    @Column(name = "ITM_RATING")
+    private Double rating;
+
+    /**
+     * id новости
+     */
+    @Column(name = "ITM_RATING_NUMBER")
+    private Long ratingNumber;
+
+    /**
+     * id новости
+     */
+    @Column(name = "ITM_SIGNATURE")
+    private String signature;
+
+    /**
+     * id новости
+     */
+    @Column(name = "ITM_STATE")
+    private String state;
+
+
+    /**
+     * id новости
+     */
+    @Column(name = "ITM_STATE_MODIFY_DATE")
+    private Date stateUpdate;
+
+
 }
